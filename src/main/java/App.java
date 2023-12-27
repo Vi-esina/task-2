14294b9e41e1267c613fc8f88c97d0cdbd69b0aa
МVi-esina <viktorija.esman@gmail.com>
2023-12-27 15:02:02 +0300

    task-2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessageCat());
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessageCat());
+
+        boolean beanCompare= (bean==bean2);
+        boolean beanCompareCat= (beanCat==beanCat2);
+        System.out.println(beanCompare);
+        System.out.println(beanCompareCat);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String messageCat;
+
+    public String getMessageCat() {
+        return messageCat;
+    }
+
+    public void setMessageCat(String messageCat) {
+        this.messageCat = messageCat;
+    }
+}
